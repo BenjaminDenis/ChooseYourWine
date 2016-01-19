c15ba8ee6819693e6dcdd00ca5c3263ca34467f8
BenjaminDenis <bd.benjamin.denis@gmail.com>
2016-01-19 10:01:48 +0100

    Create project ChooseYourWine

---
+++ app/src/main/java/com/epsi/projects/chooseyourwine/RecognitionActivity.java
@@ -0,0 +1,13 @@
+package com.epsi.projects.chooseyourwine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecognitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recognition);
+    }
+}

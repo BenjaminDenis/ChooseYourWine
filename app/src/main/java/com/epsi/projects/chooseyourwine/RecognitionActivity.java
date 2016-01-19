68e6fe4b89559181b39a722b9d1b434ece94f1f9
BenjaminDenis <bd.benjamin.denis@gmail.com>
2016-01-19 11:54:58 +0100

    Add Google Barcode API

--- app/src/main/java/com/epsi/projects/chooseyourwine/RecognitionActivity.java
+++ app/src/main/java/com/epsi/projects/chooseyourwine/RecognitionActivity.java
@@ -1,7 +1,18 @@
 package com.epsi.projects.chooseyourwine;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class RecognitionActivity extends AppCompatActivity {
 
@@ -9,5 +20,41 @@ public class RecognitionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recognition);
+
+        // Button to launch recognition
+        Button btn = (Button) findViewById(R.id.bLaunchReco);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        // Load QRCode image for test
+        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+        Bitmap myBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.test_qrcode);
+        myImageView.setImageBitmap(myBitmap);
+
+        // Get message zone
+        TextView tvMessage = (TextView) findViewById(R.id.txtMessage);
+
+        // Setup Barcode detector
+        BarcodeDetector detector =
+                new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+        if(!detector.isOperational()){
+            tvMessage.setText("Could not set up the detector!");
+            return;
+        }
+
+        // Detect Barcode
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+
+        // Decode Barcode
+        Barcode thisCode = barcodes.valueAt(0);
+        tvMessage.setText(thisCode.rawValue);
     }
 }

0c71f318a5c6cece7604186f496a2686bb8ce1c4
BenjaminDenis <bd.benjamin.denis@gmail.com>
2016-03-04 09:32:33 +0100

    package activities

---
+++ app/src/main/java/com/epsi/projects/chooseyourwine/activities/ListWinesActivity.java
@@ -0,0 +1,14 @@
+package com.epsi.projects.chooseyourwine.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Benjamin on 04/03/2016.
+ */
+public class ListWinesActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
